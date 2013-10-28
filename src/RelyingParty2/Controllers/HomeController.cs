aaa096abd4861b56ccf18281ca388ddad2457bf4
Pedro Felix <pmhsfelix@gmail.com>
2013-10-28 21:33:20 +0000

    Adds RP as defined in 2013/10/25 lecture

---
+++ src/RelyingParty2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RelyingParty2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            var princ = User as ClaimsPrincipal;
+            var sb = new StringBuilder();
+            sb.Append("<html><body><ul>");
+            foreach (var claim in princ.Claims)
+            {
+                sb.Append(string.Format("<li>{0} - {1}</li>", claim.Type, claim.Value));
+            }
+            sb.Append("</ul></body></html>");
+            return sb.ToString();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
